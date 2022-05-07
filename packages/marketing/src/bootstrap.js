f4627eed24ebcc0039602ea8d5670b91b62c1c57
CameronFu <531019326@qq.com>
2022-05-08 00:29:24 +0800

    sync router history between container and market

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
-const mount = (element) => {
-  ReactDom.render(<App />, element);
+
+const mount = (element, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  onNavigate && history.listen(onNavigate);
+  ReactDom.render(<App history={history} />, element);
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathName) {
+        history.push(nextPathName);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === 'development') {
   const element = document.querySelector('#_marketing-dev-root');
-  element && mount(element);
+  element && mount(element, { defaultHistory: createBrowserHistory() });
 }
 
 export { mount };
